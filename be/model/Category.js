b49dc13a25a9feb9bbf744c8ea110d8d7979c40e
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-05-24 18:48:12 +0700

    rev 1848

--- be/model/Category.js
+++ be/model/Category.js
@@ -3,22 +3,36 @@ import db from "../config/database.js";
 export const Category = {
   create: async (categoryData) => {
     const { nama_kategori, jenis, deskripsi } = categoryData;
-    const query = "INSERT INTO categories (nama_kategori, jenis, deskripsi) VALUES (?, ?, ?)";
+    const query =
+      "INSERT INTO categories (nama_kategori, jenis, deskripsi) VALUES (?, ?, ?)";
     return db.query(query, [nama_kategori, jenis, deskripsi]);
   },
 
   getAll: async () => {
-    const query = "SELECT * FROM categories";
+    const query = "SELECT * FROM categories ORDER BY nama_kategori ASC";
     return db.query(query);
   },
 
+  getByNama: async (nama_kategori) => {
+    const query = "SELECT * FROM categories WHERE nama_kategori = ?";
+    return db.query(query, [nama_kategori]);
+  },
+
   getByJenis: async (jenis) => {
-    const query = "SELECT * FROM categories WHERE jenis = ?";
+    const query =
+      "SELECT * FROM categories WHERE jenis = ? ORDER BY nama_kategori ASC";
     return db.query(query, [jenis]);
+  },
+
+  update: async (id, categoryData) => {
+    const { nama_kategori, jenis, deskripsi } = categoryData;
+    const query =
+      "UPDATE categories SET nama_kategori = ?, jenis = ?, deskripsi = ? WHERE id = ?";
+    return db.query(query, [nama_kategori, jenis, deskripsi, id]);
   },
 
   delete: async (id) => {
     const query = "DELETE FROM categories WHERE id = ?";
     return db.query(query, [id]);
-  }
-}; 
+  },
+};
